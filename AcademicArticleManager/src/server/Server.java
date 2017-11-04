88cd65daad4a755d9fa3c4f58e9cc19b9da33391
kenter1 <kenteralano1@gmail.com>
2017-11-04 17:44:05 -0400

    Add files via upload

---
+++ AcademicArticleManager/src/server/Server.java
@@ -0,0 +1,49 @@
+package server;
+
+public class Server {
+
+	getSearchResult() {
+	}
+	
+	boolean addUserFavorite() {
+		return false;
+	}
+	
+	void parseArticle() {
+	}
+	
+	void verifyArticle() {
+	}
+	
+	fixArticle() {
+	}
+	
+	showUploadErrors() {
+	}
+	
+	addArticleToDatabase() {
+	}
+	
+	verifyUser() {
+	}
+	
+	
+	verifyAdmin(){
+		
+	}
+	
+	createCategory(){
+		
+	}
+	
+	registerNewUser(){
+		
+	}
+	
+	updateUser(){
+		
+	}
+	
+	
+	
+}
