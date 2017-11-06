012854ce2cf6771518c103cbbfa05ae4db033369
Carson Miller <carsondm@gmail.com>
2017-11-05 21:18:07 -0500

    Results page with styles
    Website now has two pages with features. Still just a skeleton for the
    most part

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AAM | Results' });
+  res.render('index', { title: 'AAM | Index' });
 });
+
+/* this is a hack, change this */
+/* hitting the submit button on the index peforms action /results via
+	POST, but doesn't send any data currently. This is a quick hack for
+	testing purposes
+ */
+router.post('/results', function(req, res, next) {
+  res.render('results', { title: 'AAM | Results' });
+});
+
 
 module.exports = router;
