3e3a364de269e4f37f03e11250542c19545e8eda
kenter1 <kenteralano1@gmail.com>
2017-11-09 01:01:49 -0500

    Begone Thot

---
+++ src/main/java/Category.java
@@ -0,0 +1,18 @@
+/**
+ * Created by gunbo on 11/9/2017.
+ */
+public class Category {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Category(String title, String body){
+
+    }
+
+
+
+
+
+}
